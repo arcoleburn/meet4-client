909b87548807f4703c0fe8724cc1b018dfa37374
Andrew Coleburn <arcoleburn@gmail.com>
2021-01-16 19:49:30 -0500

    link to prod

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://fast-shelf-63629.herokuapp.com/api',
   TOKEN_KEY: process.env.TOKEN_KEY,
 };
