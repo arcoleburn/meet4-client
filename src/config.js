d5ab6652824cecd1e295ed606e23db91b8324dbf
Andrew Coleburn <arcoleburn@gmail.com>
2021-01-23 11:34:31 -0500

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
 export default {
-  // API_ENDPOINT: 'https://fast-shelf-63629.herokuapp.com/api',
-API_ENDPOINT: 'http://localhost:8000/api'
+  API_ENDPOINT: 'https://fast-shelf-63629.herokuapp.com/api',
+// API_ENDPOINT: 'http://localhost:8000/api'
 };
